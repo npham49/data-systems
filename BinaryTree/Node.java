a3a3a181e036b63ad7a3706598bf761ff2d1f9fc
npham49 <63203684+npham49@users.noreply.github.com>
2020-11-01 22:25:44 -0800

    Add files via upload

--- BinaryTree/Node.java
+++ BinaryTree/Node.java
@@ -1,1 +1,28 @@
-
+public class Node {
+    public int value;
+    public Node left;
+    public Node right;
+    public Node(int value) {
+        this.setValue(value);
+        this.setRight(null);
+        this.setLeft(null);
+    }
+    public int getValue(Node node) {
+        return node.value;
+    }
+    public void setValue(int value) {
+        this.value=value;
+    }
+    public Node getRight() {
+        return right;
+    }
+    public Node getLeft() {
+        return left;
+    }
+    public void setRight(Node right) {
+        this.right=right;
+    }
+    public void setLeft(Node left) {
+        this.left=left;
+    }
+}
